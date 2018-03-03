28c73a3139864e05e8ad27f71e88082e83f20aff
gokkulasudanr92 <gokkulasudanr92@gmail.com>
2018-03-02 22:30:27 -0500

    Student t Distribution PDF added

--- pgm/module_3.py
+++ pgm/module_3.py
@@ -10,6 +10,26 @@ import math
 import scipy.special as special
 import scipy.optimize as optimize
 
+def multivariate_t(X, mean, covariance, nu, D, size):
+    c = np.exp(special.gammaln((nu + D) / 2)) - special.gammaln(nu / 2)
+    c = c / (((nu * math.pi) ** (D / 2)) * (math.sqrt(np.linalg.det(covariance))))
+
+    pdf = np.zeros((1, size))
+    temp_mean = mean.reshape((D, 1))
+    for i in range(0, size):
+        temp_data = X[i, :].reshape((D, 1))
+        temp_data_minus_mean = temp_data - temp_mean
+        temp_data_minus_mean_transpose_inv_sig = np.matmul(temp_data_minus_mean.T, np.linalg.inv(covariance))
+        temp_data_minus_mean_transpose_inv_sig_data_minus_mean = np.matmul(temp_data_minus_mean_transpose_inv_sig, temp_data_minus_mean)
+        pdf[0, i] = temp_data_minus_mean_transpose_inv_sig_data_minus_mean
+
+    for i in range(0, size):
+        pdf[0, i] = 1 + (pdf[0, i] / nu)
+        pdf[0, i] = pdf[0, i] ** (-1 * (nu + D) / 2)
+        pdf[0, i] = c * pdf[0, i]
+    
+    return pdf    
+
 def near_psd(x, epsilon=0):
     '''
     Calculates the nearest postive semi-definite matrix for a correlation/covariance matrix
@@ -146,19 +166,11 @@ while True:
     for i in range(0, 1000):
         E_h_i[0, i] = (nu_k + D) / (nu_k + delta_i[0, i])
 
-    # print E_h_i.shape
-    # print E_h_i
-    # print delta_i.shape
-    # print delta_i
-
     # Compute the E[log h i]
     E_log_h_i = np.zeros((1, 1000))
     for i in range(0, 1000):
         temp = special.psi((nu_k + D) / 2) - np.log(nu_k / 2 + delta_i[0, i] / 2)
         E_log_h_i[0, i] = temp
-
-    # print E_log_h_i.shape
-    # print E_log_h_i
 
     # Update new mean
     E_h_i_sum = np.sum(E_h_i, axis = 1)
@@ -186,10 +198,6 @@ while True:
     
     # Update new nu
     new_nu = optimize.fminbound(t_cost, 0, 10, args=(E_h_i, E_h_i_sum, E_log_h_i, E_log_h_i_sum))
-    # for i in range(1, 100):
-    #     print t_cost(i, E_h_i, E_h_i_sum, E_log_h_i, E_log_h_i_sum), 
-    # print
-    # break
     nu_k = new_nu
 
     # Compute Log likelihood
@@ -234,8 +242,146 @@ faces_nu_k = nu_k
 faces_mean_k = mean_k
 faces_sig_k = sig_k
 
-print faces_nu_k
-print faces_mean_k.shape
-print faces_mean_k
-print faces_sig_k.shape
-print faces_sig_k
+## Non-faces t Distribution
+dataset_images = []
+for i in non_faces_images_rescaled_gray:
+    # for i in faces_images:
+    im_reshape = i.reshape((1, D))
+    dataset_images.append(im_reshape)
+
+dataset_matrix = np.vstack(tuple(dataset_images))
+
+non_faces_nu_k = []
+non_faces_mean_k = []
+non_faces_sig_k = []
+non_faces_covariance_epsilon = []
+
+# Initialize the teta parameters
+# Initialize Mean
+dataset_mean = np.mean(dataset_matrix, axis = 0)
+mean_k = dataset_mean
+
+# Initialize Nu to large value
+nu_k = 10
+
+# Initialize Sigma
+dataset_variance = np.zeros((D, D))
+for i in range(0, 1000):
+    x = dataset_matrix[i, :]
+    mu = mean_k
+    temp = x - mu
+    temp = temp.reshape((1, D))
+    dataset_variance = dataset_variance + np.matmul(temp.T, temp)
+
+dataset_variance /= 1000
+sig_k = dataset_variance
+
+precision = 0.01
+iterations = 0
+previous_L = 1000000
+delta_i = np.zeros((1, 1000))
+while True:
+    ## Expectation Step
+    sig_k_inverse = np.linalg.inv(sig_k)
+    for i in range(0, 1000):
+        x = dataset_matrix[i, :].reshape((1, D))
+        mu = mean_k.reshape((1, D))
+        temp = x - mu
+        temp_delta = np.matmul(temp, sig_k_inverse)
+        temp_delta = np.matmul(temp_delta, temp.T)
+        delta_i[0, i] = temp_delta
+    
+    # Compute E[h i]
+    E_h_i = np.zeros((1, 1000))
+    for i in range(0, 1000):
+        E_h_i[0, i] = (nu_k + D) / (nu_k + delta_i[0, i])
+
+    # Compute the E[log h i]
+    E_log_h_i = np.zeros((1, 1000))
+    for i in range(0, 1000):
+        temp = special.psi((nu_k + D) / 2) - np.log(nu_k / 2 + delta_i[0, i] / 2)
+        E_log_h_i[0, i] = temp
+
+    # Update new mean
+    E_h_i_sum = np.sum(E_h_i, axis = 1)
+    new_mean_k = np.zeros((1, D))
+    for i in range(0, 1000):
+        temp_xi = dataset_matrix[i, :].reshape((1, D))
+        temp_mean = mean_k.reshape((1, D))
+        temp = E_h_i[0, i] * temp_xi
+        new_mean_k += temp
+    new_mean_k /= E_h_i_sum
+    mean_k = new_mean_k
+    
+    # Update new sigma
+    new_sig_k = np.zeros((D, D))
+    for i in range(0, 1000):
+        temp_xi = dataset_matrix[i, :].reshape((D, 1))
+        temp_mean = mean_k.reshape((D, 1))
+        temp = temp_xi - temp_mean
+        temp_mul = np.matmul(temp, temp.T)
+        new_sig_k += E_h_i[0, i] * temp_mul
+    new_sig_k /= E_h_i_sum
+    sig_k = new_sig_k
+
+    E_log_h_i_sum = np.sum(E_log_h_i, axis = 1)
+    
+    # Update new nu
+    new_nu = optimize.fminbound(t_cost, 0, 10, args=(E_h_i, E_h_i_sum, E_log_h_i, E_log_h_i_sum))
+    nu_k = new_nu
+
+    # Compute Log likelihood
+    new_delta_i = np.zeros((1, 1000))
+    sig_k_inverse = np.linalg.inv(sig_k)
+    for i in range(0, 1000):
+        temp_xi = dataset_matrix[i, :].reshape((1, D))
+        temp_mean = mean_k.reshape((1, D))
+        temp_data = temp_xi - temp_mean
+        temp = np.matmul(temp_data, sig_k_inverse)
+        temp = np.matmul(temp, temp_data.T)
+        new_delta_i[0, i] = temp
+    delta_i = new_delta_i
+
+    # Compute L
+    size = 1000
+    (sig_sign, sig_k_logdet) = np.linalg.slogdet(sig_k)
+    sig_k_logdet_half = sig_k_logdet / 2
+    gammaln_nu_D_half = special.gammaln((nu_k + D) / 2)
+    log_nu_pi = (D / 2) * np.log(nu_k * math.pi)
+    gammaln_nu_half = special.gammaln(nu_k / 2)
+
+    L = size * (gammaln_nu_D_half - log_nu_pi - sig_k_logdet_half - gammaln_nu_half)
+    log_delta_nu_sum = 0
+    for i in range(0, 1000):
+        temp = np.log(1 + (delta_i[0, i] / nu_k))
+        log_delta_nu_sum += temp
+    log_delta_nu_sum /= 2
+    L = L - ((nu_k + D) * log_delta_nu_sum)
+
+    iterations += 1
+    print
+    print "Iterations Completed: " + str(iterations)
+    print
+
+    if abs(L - previous_L) < precision or iterations > 100:
+        break
+    else:
+        previous_L = L
+
+non_faces_nu_k = nu_k
+non_faces_mean_k = mean_k
+non_faces_sig_k = sig_k
+
+# print faces_nu_k
+# print faces_mean_k.shape
+# print faces_mean_k
+# print faces_sig_k.shape
+# print faces_sig_k
+
+# print non_faces_nu_k
+# print non_faces_mean_k.shape
+# print non_faces_mean_k
+# print non_faces_sig_k.shape
+# print non_faces_sig_k
+
+print multivariate_t(dataset_matrix, non_faces_mean_k, non_faces_sig_k, non_faces_nu_k, D, 1000)
